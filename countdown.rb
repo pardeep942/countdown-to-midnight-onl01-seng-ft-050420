0e0a87159aa5aab6ff9a316711bb78231df96ff7
PARDEEP KAUR <pkaur84@aol.com>
2020-05-11 00:17:06 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,12 +1,4 @@
 #write your code here
 
 def countdown
-  def countdown(number)
-  while number > 0
-   sleep 1
-    puts "#{number} SECOND(S)!"
-    number -= 1
-  end
-  return "HAPPY NEW YEAR!"
-  
   
