876e3a7c9f1d296dff04e56191f3bcb321d3f07b
PARDEEP KAUR <pkaur84@aol.com>
2020-05-11 00:17:50 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,18 @@
 #write your code here
 
-def countdown
+def countdown(count)
+    while count >0
+      puts "#{count} seconds!"
+      count-=1
+    end
+    "0 left. HAPPY NEW YEAR!"
+  end
+
+  def countdown_with_sleep(count)
+    while count > 0
+      count-=1
+      sleep(1)
+    end
+      puts "0 left. HAPPY NEW YEAR!"
+  end
   
